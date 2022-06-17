a0eb2d5781ed6b9f24e93a530a7b30c76287caa1
EWassmann <107703321+EWassmann@users.noreply.github.com>
2022-06-17 14:06:46 -0400

    Add files via upload

---
+++ ball_followarduino.ino
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+Servo servo1;
+int servoPin1 = 9;
+Servo esc;
+int servoPin2 = 10;
+
+void setup() {
+  servo1.attach(servoPin1);
+  esc.attach (servoPin2);
+  Serial.begin(2000000);
+  while(!Serial){
+    ;//WAITING FOR THE Serial TO CONNECT
+  }
+  Serial.setTimeout(200);
+esc.write(80);
+}
+
+
+int commandfromjetson;
+void loop() {
+   
+  //waiting for jetson command
+  if (Serial.available()>0){
+  commandfromjetson = Serial.readString().toInt();
+ 
+  if (commandfromjetson == 0){
+    servo1.writeMicroseconds(1500);
+    esc.write(100);
+  }
+  if (commandfromjetson == 1){
+  servo1.writeMicroseconds(2000);  
+  esc.write(100); // turn the LED on (HIGH is the voltage level)
+  }                       // wait for a second
+  if (commandfromjetson ==2){
+  servo1.writeMicroseconds(1000);   
+  esc.write(100);
+  // turn the LED off by making the voltage LOW
+  }                      // wait for a second
+
+  }  
+  
+}
